22142097c563b8b3daff2b97023bfb6c405a4e8e
sopr097 <yannick.naumann@tu-dortmund.de>
2021-06-23 12:16:49 +0200

    Used Mikails train Method. -> Accuracy ~98%

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import argparse
 import torchvision
+import torch.nn.functional as F
 from torch import nn
 import torch.optim as optim
 from torchvision import transforms, datasets
@@ -12,33 +13,43 @@ from torch.utils.data import DataLoader
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        # Berechnungspipeline
-        self.run = nn.Sequential(
-            # Layer: 784 -> 64
-            nn.Linear(28 * 28, 64),
-            nn.ReLU(),
-            # Layer: 64 -> 64
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            # Layer: 64 -> 10
-            nn.Linear(64, 10),
-            nn.ReLU(),
-        )
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-    # Berechnung
     def forward(self, x):
-        x = self.flatten(x)
-        x = self.run(x)
-        return x
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
-def train(model, train_loader, optimizer, device, epoch):
-    for data in train_loader:
-        image, label = data
-        model.zero_grad()
-        result = model(image.to(device))
-        loss = nn.functional.nll_loss(result.to(device), label.to(device))
+def train(args, model, train_loader, optimizer, device, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
         loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
 
 def test(model, device, test_loader):
     hit = 0
@@ -109,7 +120,7 @@ def main():
     # Fortschritt (UI)
     optimizer = optim.Adadelta(bnn.parameters(), lr=args.lr)
     for epoch in range(epochs):
-        train(bnn,training_set,optimizer,device,epoch)
+        train(args, bnn,training_set,optimizer,device,epoch)
         print(f"Fortschritt: {epoch+1}/{epochs}")
 
 
